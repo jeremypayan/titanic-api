f77ce3aa9cf3814838a8e7a2830fe4d9a4839f5b
jeremypayan <jeremy.payan17@gmail.com>
2021-07-11 13:25:49 +0200

    module helpers with preprocessing stuff

---
+++ sample/helpers.py
@@ -0,0 +1,41 @@
+import pandas as pd
+from sklearn.impute import SimpleImputer
+from sklearn import preprocessing
+
+def age_categories(x):
+    """Takes an age as input and returns the age category. It will be applied on Age arrays"""
+    if x == 0:
+        cat = 0
+    elif x <= 18:
+        cat = 1
+    elif x <= 60:
+        cat = 2
+    else:
+        cat = 3
+    return cat
+
+
+class DataPreparation:
+    def __init__(self, df):
+        self.df = df
+    def filling_data(self, imp):
+        array = imp.fit_transform(self.df)
+        self.df = pd.DataFrame(array, columns=self.df.columns)
+    def feature_engineering(self):
+        try:
+            le = preprocessing.LabelEncoder()
+            self.df['age_category'] = self.df.Age.apply(age_categories)
+            self.df['Sex_code'] = le.fit_transform(self.df['Sex'])
+            self.df['Pclass_code'] = le.fit_transform(self.df['Pclass'])
+            self.df['Embarked_code'] = le.fit_transform(self.df['Embarked'])
+            return self.df
+        except Exception as e:
+            print(f"error : {e}")
+    def vectorize_train_set(self):
+        X = self.df[['Pclass_code', 'Sex_code', 'Embarked_code', 'age_category']].values
+        y = self.df['Survived'].values.astype('int')
+        return X, y
+    def vectorize_test_set(self):
+        X = self.df[['Pclass_code', 'Sex_code', 'Embarked_code', 'age_category']].values
+        return X
+
